a4099db7aaf62f9c0755d6a6c8c37a5e9514d947
StanleySathler <stanleysathlerpinto@gmail.com>
2024-03-29 19:48:53 -0300

    chore: setup msw

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 // /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
-    '^.+\\.[tj]sx?$': [ // to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    // '^.+\\.tsx?$': [
-      'ts-jest',
+    "^.+\\.[tj]sx?$": [
+      // to process js/ts with `ts-jest`
+      // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+      // '^.+\\.tsx?$': [
+      "ts-jest",
       {
         // ts-jest configuration goes here
       },
