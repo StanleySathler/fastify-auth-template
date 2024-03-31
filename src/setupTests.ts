869e2e5987601f4618d05dded6293bbfaa715e2a
Stanley Sathler <stanleysathlerpinto@gmail.com>
2024-03-31 18:56:04 -0300

    chore: Connect tests to Prisma (#5)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,12 @@
 import { server } from "./mocks/server";
+import { prismaClient } from "./prisma";
 
 beforeAll(async () => {
   // Start msw server
   server.listen();
+
+  // Connect Prisma - not really needed as it has a lazy connection anyway.
+  prismaClient.$connect();
 });
 
 afterEach(async () => {
@@ -13,4 +17,7 @@ afterEach(async () => {
 afterAll(async () => {
   // Close msw after all tests.
   server.close();
+
+  // Disconnect Prisma - no reason to main it open if tests are now finished.
+  prismaClient.$disconnect();
 });
